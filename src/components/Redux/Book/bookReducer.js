ca55844c2680b96a481d59b5645dbd089e13a298
unknown <lammichhanemanoj1@gmail.com>
2021-05-21 20:23:15 +0545

    secand_commit

--- src/components/Redux/Book/bookReducer.js
+++ src/components/Redux/Book/bookReducer.js
@@ -1,50 +1,46 @@
-import {AddBook,DeleteBook, EditBook,GetBook} from './booktypes'
+import { AddBook, DeleteBook, EditBook, GetBook } from "./booktypes";
 
+const initialState = {
+  books: [
+    { id: "1", title: "let us c", author: "kk" },
+    { id: "2", title: "learnig c by exapmles", author: "knk" },
+  ],
+  book: null,
+};
 
-const initialState={
-    books:[
-        {id:"1",title:"let us c", author:"kk"},
-        {id:"2",title:"learnig c by exapmles", author:"knk"}
-    ],
-    book:null
-}
-
-
-export const bookReducer = (state=initialState,action) =>{
-
-        switch(action.type){
-            case AddBook:{
-                return{
-                    books:[...state.books,action.values]
-                }    
-            }
-            case DeleteBook:{
-                let newlist = state.books.filter((book)=>action.id !== book.id)
-                return{
-                    ...state,
-                    books:newlist,               
-                }
-            }
-            case EditBook:{   
-                return{
-                    ...state,
-                    books:state.books.map((book) =>
-                        book.id == action.value.id ? action.value : book
-                       )            
-                       }
-                }
-            case GetBook:{
-                let booktoedit = state.books.find((book)=>action.id===book.id)
-                return{
-                    ...state,
-                    book:booktoedit,
-                }
-            }
-            default:{
-                return{
-                    ...state,
-                }
-            }
-        }
-        
-}
+export const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case AddBook: {
+      return {
+        books: [...state.books, action.values],
+      };
+    }
+    case DeleteBook: {
+      let newlist = state.books.filter((book) => action.id !== book.id);
+      return {
+        ...state,
+        books: newlist,
+      };
+    }
+    case EditBook: {
+      return {
+        ...state,
+        books: state.books.map((book) =>
+          book.id === action.value.id ? action.value : book
+        ),
+      };
+    }
+    case GetBook: {
+      let booktoedit = state.books.find((book) => action.id === book.id);
+      return {
+        ...state,
+        book: booktoedit,
+      };
+    }
+    default: {
+      return {
+        ...state,
+      };
+    }
+  }
+};
--- src/components/Redux/Book/bookactions.js
+++ src/components/Redux/Book/bookactions.js
@@ -1,26 +1,26 @@
-import {AddBook,DeleteBook,EditBook,GetBook,UpdateBook} from './booktypes'
+import { AddBook, DeleteBook, EditBook, GetBook } from "./booktypes";
 
-export const ADDBOOK = (values) =>{
-    return{
-    type:AddBook,
-    values
-    }
-}
+export const ADDBOOK = (values) => {
+  return {
+    type: AddBook,
+    values,
+  };
+};
 export const DELETEBOOK = (id) => {
-    return{
-    type:DeleteBook,
-    id
-    }
-}
-export const EDITBOOK = (value) =>{
-    return{
-    type:EditBook,
-    value
-    }
-}
-export const GETBOOK =(id)=>{
-    return{
-        type:GetBook,
-        id
-    }
-}
+  return {
+    type: DeleteBook,
+    id,
+  };
+};
+export const EDITBOOK = (value) => {
+  return {
+    type: EditBook,
+    value,
+  };
+};
+export const GETBOOK = (id) => {
+  return {
+    type: GetBook,
+    id,
+  };
+};
